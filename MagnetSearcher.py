d67067ba29d02cb6ce9432e46bce74ac7c60d9c0
Wangrong23 <1902167838@qq.com>
2017-11-25 11:39:30 +0800

    Update MagnetSearcher.py

--- MagnetSearcher.py
+++ MagnetSearcher.py
@@ -3,7 +3,6 @@ import base64
 from urllib import parse
 from urllib import request
 
-
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
 }
@@ -12,10 +11,9 @@ inputKeyWord = input("输入搜索关键词：")
 bytesKeyWord = inputKeyWord.encode(encoding="utf-8")
 decodeWord = base64.b64encode(bytesKeyWord)
 keyCode = decodeWord.decode()
-urlPart1 = "http://www.btwhat.org/search/b-"
+urlPart1 = "http://www.btwhat.info/search/b-"
 urlPart2 = keyCode+"/"
 urlPart4 = "-3.html"
-
 
 def strDecode(uriCode):
     a = re.sub('[+"]', '', uriCode)
@@ -32,14 +30,11 @@ for pageNum in range(1,10):
     ret = res.read().decode("utf-8")
 
     itemTitleCode = re.findall(r'<div class="item-title">.*?decodeURIComponent\((.*?)\)\);', ret, re.S)
-    downPageUrlPart1 = "http://www.btwhat.org/wiki/"
-    downPageUrlPart2 = re.findall(r'<div class="item-title">.*?<a href="/wiki/(.*?).html" target', ret, re.S)
-    downPageUrlPart3 = ".html"
+    magnetCode = re.findall(r'<div class="item-title">.*?<a href="/wiki/(.*?).html" target', ret, re.S)
     fileType = re.findall(r'<span class=\"cpill.*?\">(.*?)</span>', ret, re.S)
     fileSize = re.findall(r'<div class="item-bar">.*?File Size.*?<b.*?>(.*?)</b>', ret, re.S)
 
     titleList = []
-    downPageUrlList = []
     fileTypeList = []
     fileSizeList = []
     magnetList = []
@@ -50,11 +45,10 @@ for pageNum in range(1,10):
 
         itemTitle=strDecode(itemTitleCode[m])
         titleList.append(itemTitle)
-        downPageUrlList.append(downPageUrlPart1+downPageUrlPart2[m])
         fileTypeList.append(fileType[m])
         fileSizeList.append(fileSize[m])
         magnetPart1 = "magnet:?xt=urn:btih:"
-        magnetPart2 = downPageUrlPart2[m]
+        magnetPart2 = magnetCode[m]
         magnet = magnetPart1+magnetPart2
         magnetList.append(magnet)
 
@@ -69,4 +63,3 @@ for pageNum in range(1,10):
         print("资源大小：" + size)
         print("磁力链接："+magnet)
         print("\r")
-
